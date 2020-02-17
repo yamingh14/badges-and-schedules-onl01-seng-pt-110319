c9ddc1688a155c3448e971823654a73261ba766a
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-02-17 03:16:48 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,7 @@
 def badge_maker(speaker)
-    return  "Hello, my name is #{speaker}."
+    "Hello, my name is #{speaker}."
 end
 
-# Method 2
-# def batch_badge_creator(names)
-#   new_array = []
-#   names.each do |speaker|
-#     new_array.push "Hello, my name is #{speaker}."
-#   end
-# new_array
-# end
 
 # Method 2
 def batch_badge_creator(names)
